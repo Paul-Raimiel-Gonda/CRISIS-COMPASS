07e39c2e43eada143cdfb08b59635183c4c659a9
Paul-Raimiel-Gonda <157487413+Paul-Raimiel-Gonda@users.noreply.github.com>
2024-10-27 20:49:51 +0800

    updated description

--- src/GAME.java
+++ src/GAME.java
@@ -4,7 +4,7 @@ import forms.*;
 //MAIN FILE
 public class GAME {
     public static void main(String[] args) {
-        JFrame frame = new JFrame("GAME: Gaming Access and Media Emporium - Login");
+        JFrame frame = new JFrame("CRISIS COMPASS: Disaster Preparation Assistant - Login");
         frame.setContentPane(new LoginForm(frame).getPanel());
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
